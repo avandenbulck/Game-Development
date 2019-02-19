ebe17e62128326337ab775191ec705771cdb91e0
a.vandenbulck <a.vandenbulck@gmail.com>
2019-02-19 13:56:57 +0100

    - Split the PlayerController script into a PlayerController and GameManager script - Made UI scalable

---
+++ My Roll-a-ball/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public PlayerController playerController;
+
+    private int count;
+
+    private void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        playerController.OnColissionEvent += IncreaseScore;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        playerController.Move(moveHorizontal, moveVertical);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        count += 1;
+        SetCountText();
+    }
+}
---
+++ My Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public delegate void OnColission();
+    public event OnColission OnColissionEvent;
+
+    private Rigidbody rb;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(float moveHorizontal, float moveVertical)
+    {
+        this.moveHorizontal = moveHorizontal;
+        this.moveVertical = moveVertical;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            OnColissionEvent.Invoke();
+        }
+    }
+}
