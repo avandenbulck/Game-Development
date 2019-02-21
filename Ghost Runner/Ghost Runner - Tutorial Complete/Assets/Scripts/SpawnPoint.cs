a317edd25ec0e377236d9ec17ba20bd9a2758c31
a.vandenbulck <a.vandenbulck@gmail.com>
2019-02-21 17:00:24 +0100

    Added Ghost Runner - Tutorial Complete

---
+++ Ghost Runner/Ghost Runner - Tutorial Complete/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    void Start()
+    {
+        Instantiate(obstacle, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
